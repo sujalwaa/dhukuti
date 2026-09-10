b7e1498fc22a555c5ece08e8fe88b90c84e800d0
Sujal <mail.hellosujal@gmail.com>
2026-09-10 08:36:04 +0545

    chore: add Supabase configuration

--- lib/core/config/supabase_config.dart
+++ lib/core/config/supabase_config.dart
@@ -1,4 +1,4 @@
 class SupabaseConfig {
-  static const String url = 'https://YOUR_PROJECT_ID.supabase.co';
-  static const String anonKey = 'YOUR_ANON_KEY';
+  static const String url = 'https://oygvyxjprgirfrhvrnhs.supabase.co';
+  static const String anonKey = 'sb_publishable_7DNfAW5uyl_QRi4g6cWY1Q_5jbp2AO9';
 }

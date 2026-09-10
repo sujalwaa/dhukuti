031aac6b72b3482a81239ed348152a50b6768503
Sujal <mail.hellosujal@gmail.com>
2026-09-10 08:58:46 +0545

    ci: configure vercel buildCommand and outputDirectory in vercel.json

--- vercel.sh
+++ vercel.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e
 echo "Downloading Flutter..."
-git clone https://github.com/flutter/flutter.git -b stable
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:`pwd`/flutter/bin"
+
+echo "Configuring Flutter..."
+flutter config --no-analytics
+flutter config --enable-web
 
 echo "Building Flutter Web App..."
 flutter pub get
